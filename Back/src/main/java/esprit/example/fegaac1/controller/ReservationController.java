75cb9812120c55a299abdc104782f3d49232b087
YassmineCherif <yasmine.cherif@esprit.tn>
2025-08-24 14:30:59 +0100

    fixed reseravtions date

--- Back/src/main/java/esprit/example/fegaac1/controller/ReservationController.java
+++ Back/src/main/java/esprit/example/fegaac1/controller/ReservationController.java
@@ -1,125 +1,125 @@
-package esprit.example.fegaac1.controller;
+    package esprit.example.fegaac1.controller;
 
 
-import esprit.example.fegaac1.entities.*;
-import esprit.example.fegaac1.repository.ReservationRepository;
-import esprit.example.fegaac1.services.IReservationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import esprit.example.fegaac1.entities.*;
+    import esprit.example.fegaac1.repository.ReservationRepository;
+    import esprit.example.fegaac1.services.IReservationService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.format.annotation.DateTimeFormat;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+    import java.time.LocalDate;
+    import java.util.List;
+    import java.util.Set;
+    import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/reservation")
-@RequiredArgsConstructor
-public class ReservationController {
+    @RestController
+    @RequestMapping("/reservation")
+    @RequiredArgsConstructor
+    public class ReservationController {
 
-    private final IReservationService reservationService;
-    private final ReservationRepository reservationRepository;
+        private final IReservationService reservationService;
+        private final ReservationRepository reservationRepository;
 
-    @GetMapping("/pays")
-    public List<Pays> getAllPays() {
-        return reservationService.getAllPays();
-    }
+        @GetMapping("/pays")
+        public List<Pays> getAllPays() {
+            return reservationService.getAllPays();
+        }
 
-    @GetMapping("/residences/{paysId}")
-    public Set<Residence> getResidences(@PathVariable Long paysId) {
-        return reservationService.getResidencesByPays(paysId);
-    }
+        @GetMapping("/residences/{paysId}")
+        public Set<Residence> getResidences(@PathVariable Long paysId) {
+            return reservationService.getResidencesByPays(paysId);
+        }
 
-    @GetMapping("/blocs/{residenceId}")
-    public Set<Bloc> getBlocs(@PathVariable Long residenceId) {
-        return reservationService.getBlocsByResidence(residenceId);
-    }
+        @GetMapping("/blocs/{residenceId}")
+        public Set<Bloc> getBlocs(@PathVariable Long residenceId) {
+            return reservationService.getBlocsByResidence(residenceId);
+        }
 
-    @GetMapping("/appartements/{blocId}")
-    public Set<Appartement> getAppartements(@PathVariable Long blocId) {
-        return reservationService.getAppartementsByBloc(blocId);
-    }
-
-
-
-    @PostMapping("/{appartementId}")
-    public Reservation createReservation(
-            @PathVariable Long appartementId,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
-    ) {
-        return reservationService.createReservation(appartementId, start, end);
-    }
-
-    @PostMapping("/reserver/{appartementId}")
-    public ResponseEntity<?> reserverAppartement(
-            @PathVariable Long appartementId,
-            @RequestParam Long userId,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateDebut,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFin
-    ) {
-        Reservation reservation = reservationService.reserverAppartement(appartementId, userId, dateDebut, dateFin);
-        return ResponseEntity.ok(reservation);
-    }
-
-
-
-    @GetMapping("/exists")
-    public ResponseEntity<Boolean> isReserved(
-            @RequestParam Long appartementId,
-            @RequestParam Long userId
-    ) {
-        boolean exists = reservationService.existsByUserAndAppartement(userId, appartementId);
-        return ResponseEntity.ok(exists);
-    }
-
-
-
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Reservation>> getReservationsByUser(@PathVariable Long userId) {
-        return ResponseEntity.ok(reservationService.getReservationsByUser(userId));
-    }
-
-
-
-    @GetMapping("/hasApproved/{userId}")
-    public boolean hasApprovedReservation(@PathVariable Long userId) {
-        return reservationService.hasApprovedReservation(userId);
-    }
-
-
-    @GetMapping("/reservations/overlap")
-    public boolean checkOverlap(
-            @RequestParam Long userId,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
-    ) {
-        return reservationService.hasOverlap(userId, start, end);
-    }
-
-
-        @PutMapping("/approval/{id}")
-        public ResponseEntity<?> updateApproval(
-                @PathVariable Long id,
-                @RequestParam int status
-        ) {
-            Reservation res = reservationService.updateApproval(id, status);
-            return ResponseEntity.ok(res);
+        @GetMapping("/appartements/{blocId}")
+        public Set<Appartement> getAppartements(@PathVariable Long blocId) {
+            return reservationService.getAppartementsByBloc(blocId);
         }
 
 
 
-    @GetMapping("/all")
-    public List<Reservation> getAllReservations() {
-        return reservationRepository.findAll();
+        @PostMapping("/{appartementId}")
+        public Reservation createReservation(
+                @PathVariable Long appartementId,
+                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
+        ) {
+            return reservationService.createReservation(appartementId, start, end);
+        }
+
+        @PostMapping("/reserver/{appartementId}")
+        public ResponseEntity<?> reserverAppartement(
+                @PathVariable Long appartementId,
+                @RequestParam Long userId,
+                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateDebut,
+                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFin
+        ) {
+            Reservation reservation = reservationService.reserverAppartement(appartementId, userId, dateDebut, dateFin);
+            return ResponseEntity.ok(reservation);
+        }
+
+
+
+        @GetMapping("/exists")
+        public ResponseEntity<Boolean> isReserved(
+                @RequestParam Long appartementId,
+                @RequestParam Long userId
+        ) {
+            boolean exists = reservationService.existsByUserAndAppartement(userId, appartementId);
+            return ResponseEntity.ok(exists);
+        }
+
+
+
+        @GetMapping("/user/{userId}")
+        public ResponseEntity<List<Reservation>> getReservationsByUser(@PathVariable Long userId) {
+            return ResponseEntity.ok(reservationService.getReservationsByUser(userId));
+        }
+
+
+
+        @GetMapping("/hasApproved/{userId}")
+        public boolean hasApprovedReservation(@PathVariable Long userId) {
+            return reservationService.hasApprovedReservation(userId);
+        }
+
+
+        @GetMapping("/reservations/overlap")
+        public boolean checkOverlap(
+                @RequestParam Long userId,
+                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
+        ) {
+            return reservationService.hasOverlap(userId, start, end);
+        }
+
+
+            @PutMapping("/approval/{id}")
+            public ResponseEntity<?> updateApproval(
+                    @PathVariable Long id,
+                    @RequestParam int status
+            ) {
+                Reservation res = reservationService.updateApproval(id, status);
+                return ResponseEntity.ok(res);
+            }
+
+
+
+        @GetMapping("/all")
+        public List<Reservation> getAllReservations() {
+            return reservationRepository.findAll();
+        }
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-}
--- Front/src/app/componment/front/reservation/apartements/apartements.component.ts
+++ Front/src/app/componment/front/reservation/apartements/apartements.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AppartementService } from 'src/app/Services/appartement/appartement.service';
-import { ReservationService } from 'src/app/Services/Reservation/reservation.service';
 import { Pays } from 'src/app/models/Pays';
+import { Residence } from 'src/app/models/Residence';
+import { Bloc } from 'src/app/models/bloc';
+import { Appartement } from 'src/app/models/appartement';
+import { ReservationService } from 'src/app/Services/Reservation/reservation.service';
+import { ToastrService } from 'ngx-toastr';
 import { Reservation } from 'src/app/models/Reservation';
+
+declare var bootstrap: any;
+
 
 @Component({
   selector: 'app-apartements',
@@ -12,36 +19,196 @@ import { Reservation } from 'src/app/models/Reservation';
 export class ApartementsComponent implements OnInit {
 
   paysList: Pays[] = [];
-  reservations: Reservation[] = [];
-  selectedCountry: string = 'all';
+  residencesList: Residence[] = [];
+  blocsList: Bloc[] = [];
+  appartementsList: Appartement[] = [];
+
+  selectedPaysId?: number;
+  selectedResidenceId?: number;
+  selectedBlocId?: number;
+  selectedAppartementId?: number;
+
+reservation: Reservation = { dateDebut: '', dateFin: '', approved: 0 };
+selectedAppartement: Appartement | null = null;
+
+
+  toasts: { message: string, type: 'success' | 'error' }[] = [];
+
+  reservedApartments: number[] = []; 
+  staticUserId = 1;
 
   constructor(
     private appService: AppartementService,
-    private reservationService: ReservationService
+    private reservationService: ReservationService,
+      private toastr: ToastrService
+
   ) {}
 
   ngOnInit(): void {
     this.fetchPays();
-    this.getReservations(); // load all reservations at start
   }
 
+  // ================== FETCH ==================
   fetchPays(): void {
     this.appService.getAllPays().subscribe({
       next: data => this.paysList = data,
-      error: () => console.error("Erreur lors du chargement des pays ❌")
+      error: () => this.showToast('Erreur lors du chargement des pays ❌', 'error')
     });
   }
 
-  getReservations(): void {
-    this.reservationService.getReservationsByCountry(this.selectedCountry).subscribe({
-      next: (data) => {
-        console.log("Fetched reservations:", data);
-        this.reservations = data;
+  onPaysChange(): void {
+    if (!this.selectedPaysId) {
+      this.residencesList = [];
+      this.blocsList = [];
+      this.appartementsList = [];
+      return;
+    }
+    this.appService.getResidencesByPays(this.selectedPaysId).subscribe({
+      next: data => { 
+        this.residencesList = data;
+        this.blocsList = [];
+        this.appartementsList = [];
+        this.selectedResidenceId = undefined;
+        this.selectedBlocId = undefined;
+        this.selectedAppartementId = undefined;
       },
-      error: (err) => {
-        console.error("Erreur lors du chargement des réservations ❌", err);
-        this.reservations = [];
-      }
+      error: () => console.error("Erreur lors du chargement des résidences ❌")
     });
+  }
+
+  onResidenceChange(): void {
+    if (!this.selectedResidenceId) {
+      this.blocsList = [];
+      this.appartementsList = [];
+      return;
+    }
+    this.appService.getBlocsByResidence(this.selectedResidenceId).subscribe({
+      next: data => { 
+        this.blocsList = data;
+        this.appartementsList = [];
+        this.selectedBlocId = undefined;
+        this.selectedAppartementId = undefined;
+      },
+      error: () => console.error("Erreur lors du chargement des blocs ❌")
+    });
+  }
+
+
+  
+
+onBlocChange(): void {
+  if (!this.selectedBlocId) {
+    this.appartementsList = [];
+    return;
+  }
+  this.appService.getAppartementsByBloc(this.selectedBlocId).subscribe({
+    next: data => {
+      this.appartementsList = data;
+      this.appartementsList.forEach(app => {
+        this.reservationService.checkReservation(app.id_app!, this.staticUserId)
+          .subscribe(isReserved => {
+            if (isReserved) this.reservedApartments.push(app.id_app!);
+          });
+      });
+    },
+    error: () => console.error("Erreur lors du chargement des appartements ❌")
+  });
+}
+
+
+
+
+
+  // ================== RESERVATION ==================
+reserver(app: Appartement): void {
+  if (!app?.id_app) {
+    this.showToast('Appartement non sélectionné ❌', 'error');
+    return;
+  }
+
+  if (!this.reservation.dateDebut || !this.reservation.dateFin) {
+    this.showToast("Veuillez choisir les dates avant de réserver ❌", "error");
+    return;
+  }
+
+  this.reservationService.reserverAppartementWithDates(
+    app.id_app,
+    this.staticUserId,
+    this.reservation.dateDebut,
+    this.reservation.dateFin
+  ).subscribe({
+    next: (res: any) => {
+      this.showToast(res.message || 'Réservation effectuée ✅', 'success');
+      this.reservedApartments.push(app.id_app!);
+    },
+    error: () => this.showToast('Erreur lors de la réservation ❌', 'error')
+  });
+}
+
+
+
+// Open modal when clicking "Réserver"
+openReservationModal(app: Appartement) {
+  this.reservationService.hasApprovedReservation(this.staticUserId).subscribe({
+    next: (hasApproved: boolean) => {
+
+      // Otherwise allow modal to open
+      this.selectedAppartement = app;
+      this.reservation = { dateDebut: '', dateFin: '', approved: 0 }; 
+      const modal = new bootstrap.Modal(document.getElementById('reservationModal')!);
+      modal.show();
+    },
+    error: () => this.showToast("Erreur lors de la vérification des réservations ❌", "error")
+  });
+}
+
+
+// Confirm reservation after selecting dates
+confirmReservation(): void {
+  if (!this.selectedAppartement || !this.reservation.dateDebut || !this.reservation.dateFin) {
+    this.showToast("Veuillez sélectionner les dates", "error");
+    return;
+  }
+
+  const userId = 1; // TODO: remplacer par user connecté
+  const appartementId = this.selectedAppartement?.id_app;
+  if (!appartementId) {
+    this.showToast("Appartement invalide ❌", "error");
+    return;
+  }
+
+  const start = this.reservation.dateDebut;
+  const end = this.reservation.dateFin;
+
+  this.reservationService.checkOverlap(userId, start, end).subscribe({
+    next: (overlap) => {
+      if (overlap) {
+        this.showToast("❌ Vous avez déjà une réservation approuvée qui chevauche ces dates", "error");
+      } else {
+        this.reservationService.reserverAppartementWithDates(appartementId, userId, start, end).subscribe({
+  next: () => {
+    this.showToast("Réservation effectuée ✅", "success");
+    (document.getElementById("reservationModal") as any).modal("hide");
+  },
+  error: () => this.showToast("Erreur lors de la réservation ❌", "error")
+});
+
+      }
+    },
+    error: () => this.showToast("Erreur lors de la vérification d'overlap ❌", "error")
+  });
+}
+
+
+
+
+
+  showToast(message: string, type: 'success' | 'error') {
+    const toast = { message, type };
+    this.toasts.push(toast);
+    setTimeout(() => {
+      const index = this.toasts.indexOf(toast);
+      if (index >= 0) this.toasts.splice(index, 1);
+    }, 3000);
   }
 }
